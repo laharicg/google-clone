9a3b51aed85b6eeb5dec16d5374b6b38967c601f
lahari govada <govada.lahari@gmail.com>
2020-08-30 17:55:14 +0530

    completed

--- google/src/keys.js
+++ google/src/keys.js
@@ -1,6 +1,4 @@
-//import
-require("dotenv").config();
-const API_KEY = "";
-console.log(process.env);
+const API_KEY = process.env.REACT_APP_API_KEY;
+//console.log(process.env);
 
 export default API_KEY;
